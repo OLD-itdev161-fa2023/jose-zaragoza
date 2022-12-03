8ca310fc0197a97dbe6ae606609a61e6d338fdac
jose-zaragoza <zaragojf@gmatc.matc.edu>
2022-12-02 22:52:16 -0600

    Completed Activity 5

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post(
                     }
                 };
 
-                jwt.sign{
+                jwt.sign(
                     payload,
                     config.get('jwtSecret'),
                     { expiresIn: '10hr' },
@@ -90,7 +90,7 @@ app.post(
                         if (err) throw err;
                         res.json({ token: token });
                     }
-                };
+                );
             } catch (error) {
                 res.status(500).send('Server error');
             }
