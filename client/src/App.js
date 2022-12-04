a614edceaa7ef67df8b0813969b4863faa57f655
jose-zaragoza <zaragojf@gmatc.matc.edu>
2022-12-03 18:55:49 -0600

    Post API Activity 10

--- client/src/App.js
+++ client/src/App.js
@@ -7,51 +7,72 @@ import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null
-  }
+  };
   
   componentDidMount() {
-    axios.get('http://localhost:5000')
-      .then((response) => {
-        this.setState({
-          data: response.data
-        })
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ${error}');
-      })
-
       this.authenticateUser();
     }
 
 authenticateUser = () => {
-  const token = localStorage.getItem('token');
+ const token = localStorage.getItem('token');
 
-  if(!token) {
-    localStorage.removeItem('user')
-    this.setState({ user: null});
-  }
-
-  if(token) {
-    const config = {
-      headers: {
-        'x-auth-token': token
-      }
-    }
-    axios.get('http://localhost:5000/api/auth', config)
-    .then((response) => {
-      localStorage.setItem('user', response.date.name)
-      this.setState({user: response.data.name })
-    })
-    .catch((error) => {
+    if (!token) {
       localStorage.removeItem('user');
       this.setState({ user: null });
-      console.error('Error logging in: ${error}');
-    })
-  }
-}
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+      axios
+        .get('http://localhost:5000/api/auth', config)
+        .then(response => {
+          localStorage.setItem('user', response.data.name);
+          this.setState(
+            {
+              user: response.data.name,
+              token: token
+            },
+            () => {
+              this.loadData();
+            }
+          );
+        })
+        .catch(error => {
+          localStorage.removeItem('user');
+          this.setState({ user: null });
+          console.error(`Error logging in: ${error}`);
+        });
+    }
+  };
+
+loadData = () => {
+    const { token } = this.state;
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+      axios
+        .get('http://localhost:5000/api/posts', config)
+        .then(response => {
+          this.setState({
+            posts: response.data
+          });
+        })
+        .catch(error => {
+          console.error(`Error fetching data: ${error}`);
+        });
+    }
+  };
 
 logOut = () => {
   localStorage.removeItem('token');
@@ -60,58 +81,84 @@ logOut = () => {
 }
 
     render() {
-      let {user, data} = this.state;
-      const authProps = {
-        authenticateUser: this.authenticateUser
-      }
+      let { user, posts, post, token } = this.state;
+    const authProps = {
+      authenticateUser: this.authenticateUser
+    };
 
       return (
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <h1>GoodThings</h1>
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
+          <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>GoodThings</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                {user ? (
+                  <Link to="/new-post">New Post</Link>
+                ) : (
                   <Link to="/register">Register</Link>
-                </li>
-                <li>
-                  {user ?
-                  <Link to="" onClick={this.logOut}>Log Out</Link> :
-                  <Link to="/login">Log In</Link>
-                  }
-
-                </li>
-              </ul>
-            </header>
-            <main>
+                )}
+              </li>
+              <li>
+                {user ? (
+                  <Link to="" onClick={this.logOut}>
+                    Log out
+                  </Link>
+                ) : (
+                  <Link to="/login">Log in</Link>
+                )}
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Switch>
               <Route exact path="/">
-                {user ?
+                {user ? (
                   <React.Fragment>
                     <div>Hello {user}!</div>
-                    <div>{data}</div>
-                    </React.Fragment> :
-                  <React.Fragment>
-                    Please Register or Login
+                    <PostList
+                      posts={posts}
+                      clickPost={this.viewPost}
+                      deletePost={this.deletePost}
+                      editPost={this.editPost}
+                    />
                   </React.Fragment>
-                }
-
+                ) : (
+                  <React.Fragment>Please Register or Login</React.Fragment>
+                )}
               </Route>
-              <Switch>
-                <Route 
-                  exact path="/register"
-                  render={() => <Register {...authProps} />} />
-                <Route
-                  exact path="/login"
-                  render={() => <Login {...authProps} />} />
-              </Switch>
-            </main>
-          </div>
+              <Route path="/posts/:postId">
+                <Post post={post} />
+              </Route>
+              <Route path="/new-post">
+                <CreatePost token={token} onPostCreated={this.onPostCreated} />
+              </Route>
+              <Route path="/edit-post/:postId">
+                <EditPost
+                  token={token}
+                  post={post}
+                  onPostUpdated={this.onPostUpdated}
+                />
+              </Route>
+              <Route
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={() => <Login {...authProps} />}
+              />
+            </Switch>
+          </main>
+        </div>
       </Router>
-      );
-    }
+    );
+  }
 }
 
 export default App;
